6cbe50c33122125dcddccf6752e898b68f48e1df
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2018-02-19 16:40:00 +0000

    Added resistance

--- viscad.py
+++ viscad.py
@@ -1,3 +1,13 @@
+'''
+viscad (c) University of Manchester 2018
+
+viscad is licensed under the MIT License.
+
+To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
+
+@author:  Pablo Carbonell, SYNBIOCHEM
+@description: DoE-based pathway libraries visualisation
+'''
 import svgwrite
 from svgwrite import cm, mm
 import xml.etree.ElementTree as ET
@@ -24,7 +34,26 @@ class Part:
             }
         for key in kwargs:
             self.kwargs[key] = kwargs[key]
-            
+        self.part = []
+
+
+class Title(Part):
+    def __init__(self, title, x=0, y=0, width=0, partid=None, **kwargs):
+        Part.__init__(self, **kwargs)
+        if partid is None:
+            pid = 'title' + str(self._partid)
+        else:
+            pid = partid
+        self.x = x 
+        self.y = y
+        self.width = width
+        self.height = 0
+        self.i = self.x
+        self.o = self.x + self.width 
+        g = svgwrite.container.Group(id=pid, **self.kwargs)
+        g.add( svgwrite.text.Text(title, insert=( self.x, self.y), stroke='none', fill='#000000', font_size='24') )
+        self.part.append( g )
+        
 
 class Cds(Part):
     def __init__(self, x=0, y=0, partid=None, **kwargs):
@@ -35,7 +64,6 @@ class Cds(Part):
         y2 = 50
         y3 = 65
         Part.__init__(self, **kwargs)
-        self.part = []
         p1 = ( ('M', x1, y3), ('L', x2, y3), ('L', x3, y2), ('L', x2, y1), ('L', x1, y1), ('L', x1, y3), ('Z',) )
         pd1 = shiftPath(p1, x, y-y2)
         if partid is None:
@@ -70,7 +98,6 @@ class Promoter(Part):
     """ Define using path """
     def __init__(self, x=0, y=0, partid=None, **kwargs):
         Part.__init__(self, **kwargs)
-        self.part = []
         p1 = ( ('M', 31.5, 15.5), ('L', 40, 23), ('L', 31.5, 30.333) )
         p2 = ( ('M', 10, 50), ('L', 10, 23), ('L', 39, 23) )
         pd1 = shiftPath(p1, x, y-50)
@@ -114,7 +141,6 @@ class Terminator(Part):
     """ Define using path """
     def __init__(self, x=0, y=0, **kwargs):
         Part.__init__(self, **kwargs)
-        self.part = []
         p1 = ( ('M', 25, 50), ('L', 25, 26) )
         p2 = ( ('M', 10, 25), ('L', 40, 25) )
         pd1 = shiftPath(p1, x, y-50)
@@ -159,9 +185,6 @@ class Rbs:
 
 def readExample(f, f2):
     """ Read the DoE file """
-#    dn = '14'
-#    f = '/mnt/syno/shared/Designs/SBCDE000%s/Design/SBCDE000%s_Doe_48lib_v4/SBCDE000%s_Doe_48lib_v4.j0' % (dn,dn,dn)
-#    f2 = '/mnt/syno/shared/Designs/SBCDE000%s/Design/SBCDE000%s_Doe_48lib_v4/SBCDE000%s_Doe_48lib_v4.ji0' % (dn,dn,dn)
     lib = []
     libid = []
     with open(f) as handler:
@@ -181,10 +204,13 @@ def readExample(f, f2):
                 libid.append( ll )
     return lib, libid
             
-def addConstruct(dwg, construct, base, cell, constructid=None):
+def addConstruct(dwg, construct, base, cell, slot, constructid=None):
+    parts = []
     colors = ['red', 'blue', 'green', 'cyan', 'magenta', 'grey', 'lavender', 'darksalmon', 'chartreuse']
     cid = construct[0]
-    parts = []
+    title = Title( cid, cell, base+0.5*slot, cell )
+    parts.append( title )
+    base += slot
     cursor = 1
     for i in range(1, len(construct)):
         try:
@@ -208,6 +234,15 @@ def addConstruct(dwg, construct, base, cell, constructid=None):
             pcolor = colors [ level - 1]
             ori1 = Origin(x=cursor*cell, y=base, partid=partid, fill=pcolor)
             parts.append( ori1 )
+            cursor += 2
+
+        if ptype == 'resistance':
+
+            pcolor = colors[ pnum ]
+            cds1 = Cds(x=cursor*cell, y=base, partid=partid, fill=pcolor)
+            conn1 = connect(parts[-1], cds1)
+            parts.append(conn1)
+            parts.append(cds1)
             cursor += 2
            
 
@@ -257,16 +292,36 @@ def createCad(f1, f2, outfile):
             constructid = libid[i]
         except:
             constructid = libid
-        parts = addConstruct(dwg, construct, (i+1)*slot, cell, constructid)
+        parts = addConstruct(dwg, construct, base=(2*i+0.5)*slot, cell=cell, slot=slot, constructid=constructid)
         i += 1
         for pc in parts:
             for p in pc.part:
                 dwg.add( p )
         w = max(w, pc.x+pc.width)                         
-    dwg.viewbox(width=w+cell, height=slot*(i+1))
+    dwg.viewbox(width=w+cell, height=slot*(2*i+0.5))
     dwg.save()
 
-        
+
+def makeReport(pdfile, design='SBC', size=10):
+    texfile = re.sub('\.pdf$', '_report.tex', pdfile)
+    mask = {'design': design, 'comment': 'Library size={}'.format(size)}
+    with open('template.tex') as handler, open(texfile, 'w') as h2:
+        for line in handler:
+            for x in mask:
+                line = re.sub('{{'+x+'}}', mask[x], line)
+            if line.startswith('\end{document}'):
+                for i in range(0, size / 10):
+                    d = ((10 - size) % 10 ) % 10
+                    y1 = max(0, 1500*( (size/10) - i ) - d * 1500/10)
+                    y2 = 1500*i
+                    tx = '\\includegraphics[width=\\textwidth, trim={{ 0 {y1} 0 {y2} }}, clip]{{{pdf}}}\n'
+                    h2.write(tx.format(y1=y1,
+                                       y2=y2,
+                                       pdf=os.path.basename(pdfile)))
+            h2.write( line )
+    p = subprocess.Popen( ['pdflatex', os.path.abspath(texfile)], cwd=os.path.dirname( pdfile ) )
+            
+
 def arguments():
     parser = argparse.ArgumentParser(description='Visual DoE. Pablo Carbonell, SYNBIOCHEM, 2018')
     parser.add_argument('doeFile', 
@@ -279,10 +334,16 @@ def arguments():
                         help='Do not generate pdf')
     parser.add_argument('-l', default=None,
                         help='Log file')
+    parser.add_argument('-r', action='store_true',
+                        help='Report')
+    parser.add_argument('-d', default=None,
+                        help='Design')
+    parser.add_argument('-s', default=10,
+                        help='Size')
     return parser
 
 
-def runViscad(args=None):
+def runViscad(args=None, report=False):
     parser = arguments()
     if args is None:
         arg = parser.parse_args()
@@ -294,10 +355,16 @@ def runViscad(args=None):
         outpdfile = os.path.join(arg.O, name+'.pdf')
     else:
         outfile = os.path.join(os.path.dirname(arg.doeFile), name+'.svg')
-        outpdfile = os.path.join(os.path.dirname(arg.doeFile), name+'.pdf')        
+        outpdfile = os.path.join(os.path.dirname(arg.doeFile), name+'.pdf')
+    
     createCad(arg.doeFile, arg.i, outfile)
     if arg.p:
         p = subprocess.call( ['/usr/bin/inkscape', outfile, '-A', outpdfile] )
+        if report:
+            makeReport( outpdfile, report['design'], report['size'] )
+        elif arg.r:
+            makeReport( outpdfile, arg.d, int(arg.s) )
+            
     if arg.l is not None:
         with open(arg.l, 'a') as handler:
             if args is None:
